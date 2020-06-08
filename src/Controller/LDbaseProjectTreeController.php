78c6e4d6e24734dc52de91dd5a9218ed7f4402f1
Bryan J. Brown <bjbrown@fsu.edu>
2020-06-08 11:47:51 -0400

    Adds Project Tree block

--- src/Controller/LDbaseProjectTreeController.php
+++ src/Controller/LDbaseProjectTreeController.php
@@ -13,14 +13,21 @@ use Drupal\Core\Render\Markup;
 class LDbaseProjectTreeController extends ControllerBase {
 
 
-  public function output_tree($project_uuid) {
+  public static function output_tree($project_uuid, $current_uuid = NULL) {
+    if ($current_uuid) {
+      $current_object = \Drupal::service('ldbase.object_service')->getLdbaseObjectFromUuid($current_uuid);
+      $current_object_id = $current_object->id();
+    }
+    else {
+      $current_object_id = FALSE;
+    }
 
-    $node = \Drupal::service('ldbase.object_service')->getLdbaseObjectFromUuid($project_uuid);
-    $project = \Drupal::service('ldbase.object_service')->getLdbaseRootProjectNodeFromLdbaseObjectNid($node->id());
+    $project_node = \Drupal::service('ldbase.object_service')->getLdbaseObjectFromUuid($project_uuid);
+    $project = \Drupal::service('ldbase.object_service')->getLdbaseRootProjectNodeFromLdbaseObjectNid($project_node->id());
 
     $tree = "<div id='project-tree-view-wrapper' class='project-tree-view'><ul id='project-tree-view-list-root' class='project-tree-view'><li id='project-tree-view-root-item project-tree-view-item-link' class='project-tree-view'><strong>Project:</strong> <a href='/projects/{$project->uuid()}'>{$project->getTitle()}<a/>";
 
-    $tree .= $this->getAffiliatedChildrenAsHtmlList($project->id());
+    $tree .= \Drupal\ldbase_content\Controller\LDbaseProjectTreeController::getAffiliatedChildrenAsHtmlList($project->id(), $current_object_id);
 
     $tree .= "</li></ul></div>";
 
@@ -29,41 +36,44 @@ class LDbaseProjectTreeController extends ControllerBase {
     ];
   }
 
-  public function getAffiliatedChildrenAsHtmlList($id) {
+  public static function getAffiliatedChildrenAsHtmlList($parent_project_id, $current_object_id = NULL) {
 
     $list = "<ul class='project-tree-view project-tree-view-list'>";
 
     $datasets_query = \Drupal::entityQuery('node')
       ->condition('type','dataset')
-      ->condition('field_affiliated_parents', $id);
+      ->condition('field_affiliated_parents', $parent_project_id);
     $datasets_result = $datasets_query->execute();
     foreach ($datasets_result as $result) {
       $node = node_load($result);
-      $list .= "<li class='project-tree-view project-tree-view-item project-tree-view-item-link'><strong>Dataset:</strong> <a href='/datasets/{$node->uuid()}'>{$node->getTitle()}</a>";
-      $list .= $this->getAffiliatedChildrenAsHtmlList($node->id());
+      $current_object_class = ($node->id() == $current_object_id ? ' project-tree-view-item-current' : '');
+      $list .= "<li class='project-tree-view project-tree-view-item project-tree-view-item-link{$current_object_class}'><strong>Dataset:</strong> <a href='/datasets/{$node->uuid()}'>{$node->getTitle()}</a>";
+      $list .= \Drupal\ldbase_content\Controller\LDbaseProjectTreeController::getAffiliatedChildrenAsHtmlList($node->id(), $current_object_id);
       $list .= "</li>";
     }
 
     $code_query = \Drupal::entityQuery('node')
       ->condition('type','code')
-      ->condition('field_affiliated_parents', $id);
+      ->condition('field_affiliated_parents', $parent_project_id);
     $code_result = $code_query->execute();
     foreach ($code_result as $result) {
       $node = node_load($result);
-      $list .= "<li class='project-tree-view project-tree-view-item project-tree-view-item-link'><strong>Code:</strong> <a href='/code/{$node->uuid()}'>{$node->getTitle()}</a>";
-      $list .= $this->getAffiliatedChildrenAsHtmlList($node->id());
+      $current_object_class = ($node->id() == $current_object_id ? ' project-tree-view-item-current' : '');
+      $list .= "<li class='project-tree-view project-tree-view-item project-tree-view-item-link{$current_object_class}'><strong>Code:</strong> <a href='/code/{$node->uuid()}'>{$node->getTitle()}</a>";
+      $list .= \Drupal\ldbase_content\Controller\LDbaseProjectTreeController::getAffiliatedChildrenAsHtmlList($node->id(), $current_object_id);
       $list .= "</li>";
     }
 
 
     $documents_query = \Drupal::entityQuery('node')
       ->condition('type','document')
-      ->condition('field_affiliated_parents', $id);
+      ->condition('field_affiliated_parents', $parent_project_id);
     $documents_result = $documents_query->execute();
     foreach ($documents_result as $result) {
       $node = node_load($result);
-      $list .= "<li class='project-tree-view project-tree-view-item project-tree-view-item-link'><strong>Document:</strong> <a href='/documents/{$node->uuid()}'>{$node->getTitle()}</a>";
-      $list .= $this->getAffiliatedChildrenAsHtmlList($node->id());
+      $current_object_class = ($node->id() == $current_object_id ? ' project-tree-view-item-current' : '');
+      $list .= "<li class='project-tree-view project-tree-view-item project-tree-view-item-link{$current_object_class}'><strong>Document:</strong> <a href='/documents/{$node->uuid()}'>{$node->getTitle()}</a>";
+      $list .= \Drupal\ldbase_content\Controller\LDbaseProjectTreeController::getAffiliatedChildrenAsHtmlList($node->id(), $current_object_id);
       $list .= "</li>";
     }
 
---
+++ src/Plugin/Block/LDbaseProjectTreeBlock.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace Drupal\ldbase_content\Plugin\Block;
+
+use Drupal\Core\Block\BlockBase;
+use Drupal\Core\Render\Markup;
+
+/**
+ * Provides a "Project Tree" block.
+ *
+ * @Block(
+ *   id="project_tree_block",
+ *   admin_label = @Translation("Project Tree Block"),
+ *   category = @Translation("LDbase Block")
+ * )
+ */
+class LDbaseProjectTreeBlock extends BlockBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function build() {
+    //$node = \Drupal::routeMatch()->getParameter('node');
+
+    $url = \Drupal::request()->getRequestUri();
+    $ldbase_object_uuid = \Drupal::service('ldbase.object_service')->isUrlAnLdbaseObjectUrl($url);
+    if ($ldbase_object_uuid) {
+      $node = \Drupal::service('ldbase.object_service')->getLdbaseObjectFromUuid($ldbase_object_uuid);
+      $parent_project_node = \Drupal::service('ldbase.object_service')->getLdbaseRootProjectNodeFromLdbaseObjectNid($node->id());
+      $tree_view = \Drupal\ldbase_content\Controller\LDbaseProjectTreeController::output_tree($parent_project_node->uuid(), $node->uuid());
+      return $tree_view;
+    }
+    else {
+      return FALSE;
+    }
+ 
+
+  }
+
+  public function getCacheMaxAge() {
+    return 0;
+  }
+
+}
