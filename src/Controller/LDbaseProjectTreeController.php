6a39cc88c0ebead23717dd7d59f908190b9a8118
Brian Arsenault <barsenault@fsu.edu>
2020-11-06 13:11:34 -0500

    adds options for 'Attach to' select fields

--- src/Controller/LDbaseProjectTreeController.php
+++ src/Controller/LDbaseProjectTreeController.php
@@ -12,7 +12,6 @@ use Drupal\Core\Render\Markup;
  */
 class LDbaseProjectTreeController extends ControllerBase {
 
-
   public static function output_tree($project_uuid, $current_uuid = NULL) {
     if ($current_uuid) {
       $current_object = \Drupal::service('ldbase.object_service')->getLdbaseObjectFromUuid($current_uuid);
@@ -82,6 +81,72 @@ class LDbaseProjectTreeController extends ControllerBase {
     $list .= "</ul>";
 
     return $list;
+  }
+
+  public static function output_select_options($project_node, $current_node = NULL) {
+    $options[$project_node->id()] = ucfirst($project_node->bundle()) . ': ' . $project_node->getTitle();
+    $children = \Drupal\ldbase_content\Controller\LDbaseProjectTreeController::getAffiliatedChildrenAsOptionArray($project_node->id(), $current_node->id());
+    foreach ($children as $key => $value) {
+      $options[$key] = $value;
+    }
+    return $options;
+  }
+
+  public static function getAffiliatedChildrenAsOptionArray($parent_project_id, $current_object_id = NULL, $option_prefix = NULL) {
+    // indent for select options
+    $option_prefix = empty($option_prefix) ? '--' : $option_prefix . '--';
+
+    $datasets_query = \Drupal::entityQuery('node')
+      ->condition('type','dataset')
+      ->condition('field_affiliated_parents', $parent_project_id);
+    $datasets_result = $datasets_query->execute();
+    foreach ($datasets_result as $result) {
+      $node = node_load($result);
+      // do not add self or children to options to prevent from becoming your own grandpa
+      if ($node->id() != $current_object_id) {
+        $options[$node->id()] = $option_prefix . ucfirst($node->bundle()) . ': ' . $node->getTitle();
+        $child_options = \Drupal\ldbase_content\Controller\LDbaseProjectTreeController::getAffiliatedChildrenAsOptionArray($node->id(), $current_object_id, $option_prefix);
+        foreach ($child_options as $key => $value) {
+          $options[$key] = $value;
+        }
+      }
+    }
+
+    $code_query = \Drupal::entityQuery('node')
+      ->condition('type','code')
+      ->condition('field_affiliated_parents', $parent_project_id);
+    $code_result = $code_query->execute();
+    foreach ($code_result as $result) {
+      $node = node_load($result);
+      // do not add self or children to options to prevent from becoming your own grandpa
+      if ($node->id() != $current_object_id) {
+        $options[$node->id()] = $option_prefix . ucfirst($node->bundle()) . ': ' . $node->getTitle();
+        $child_options = \Drupal\ldbase_content\Controller\LDbaseProjectTreeController::getAffiliatedChildrenAsOptionArray($node->id(), $current_object_id, $option_prefix);
+        foreach ($child_options as $key => $value) {
+          $options[$key] = $value;
+        }
+      }
+    }
+
+
+    $documents_query = \Drupal::entityQuery('node')
+      ->condition('type','document')
+      ->condition('field_affiliated_parents', $parent_project_id);
+    $documents_result = $documents_query->execute();
+    foreach ($documents_result as $result) {
+      $node = node_load($result);
+      // do not add self or children to options to prevent from becoming your own grandpa
+      if ($node->id() != $current_object_id) {
+        $doc_type = \Drupal::service('ldbase.object_service')->isLdbaseCodebook($node->uuid()) ? 'Codebook' : 'Document';
+        $options[$node->id()] = $option_prefix . $doc_type . ': ' . $node->getTitle();
+        $child_options = \Drupal\ldbase_content\Controller\LDbaseProjectTreeController::getAffiliatedChildrenAsOptionArray($node->id(), $current_object_id, $option_prefix);
+        foreach ($child_options as $key => $value) {
+          $options[$key] = $value;
+        }
+      }
+    }
+
+    return $options;
   }
 
 }
