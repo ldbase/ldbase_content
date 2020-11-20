753e76e5e37c19d18fbac960249b4dc711771183
Favenzio Calvo <fcalvo@admin.fsu.edu>
2020-11-19 21:15:45 -0500

    Changed footer block content.

--- src/Plugin/Block/LDbaseFooterBlock.php
+++ src/Plugin/Block/LDbaseFooterBlock.php
@@ -23,8 +23,7 @@ class LDbaseFooterBlock extends BlockBase {
     $content = <<<EOM
 <div id="ldbase-footer-block-wrapper" id="ldbase-footer-block">
   <h5>About LDbase</h5>
-  <p>This repository is a collaboration between the <a href="https://psy.fsu.edu/">FSU Department of Psychology</a> and the <a href="https://www.lib.fsu.edu/">FSU Libraries</a>.
-  It is based on work supported by the Eunice Kennedy Shriver National Institute of Child Health and Human Development under R01HD095193. The content is solely the responsibility of the authors and does not necessarily represent the official views of the National Institutes of Health.</p>
+  <p>Welcome to LDbase, a NIH-funded collaboration between researchers and librarians to build a first-of-its-kind behavioral data repository containing decades of knowledge from educational and developmental sciences on individuals across the full range of abilities. LDbase is an open science resource for the educational and developmental science scientific communities, providing a secure place to store and access data and access materials about aspects of data management and analyses. The aim of LDbase is to be a powerful resource that opens up new areas of research and accelerates discoveries, inspiring innovative research that helps us understand how individuals learn, develop, are different from each other, learn new languages, respond to interventions, and interact with their communities.</p>
   <p>You may cite LDbase: Hart, S.A., Schatschneider, T.R. Reynolds, F.E. Calvo, B. J. Brown, B. Arsenault, M.R.K. Hall, W. van Dijk, A.A. Edwards, J.A. Shero, R. Smart & J.S. Phillips (2020). <em>LDbase</em>. <a href="http://doi.org/10.33009/ldbase">http://doi.org/10.33009/ldbase</a>.</p>
 </div>
 EOM;
