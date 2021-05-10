074f49a25294ef30338966427b843b52c37c1f16
Brian Arsenault <barsenault@fsu.edu>
2021-05-10 15:45:40 -0400

    adds alt text to icons

--- src/Plugin/Block/LDbaseExploreRow.php
+++ src/Plugin/Block/LDbaseExploreRow.php
@@ -22,25 +22,25 @@ class LDbaseExploreRow extends BlockBase {
     $content = <<<EOM
 <div class="browse-link-row d-flex flex-row justify-content-around flex-wrap">
   <div class="link-block" id="block-1">
-  <img src="/modules/custom/ldbase_content/images/bookshelf.svg">
+  <img src="/modules/custom/ldbase_content/images/bookshelf.svg" alt="bookshelf icon">
   <h3>Browse Data</h3>
   <p><a class="link-div" href="/browse-data">See what's in LDbase</a></p>
   </div>
 
   <div class="link-block" id="block-2">
-  <img src="/modules/custom/ldbase_content/images/signpost-2.svg">
+  <img src="/modules/custom/ldbase_content/images/signpost-2.svg" alt="signpost icon">
   <h3>Resources</h3>
   <p><a href="/resources">Learn about good data practices</a></p>
   </div>
 
   <div class="link-block" id="block-3">
-  <img src="/modules/custom/ldbase_content/images/binoculars-fill.svg">
+  <img src="/modules/custom/ldbase_content/images/binoculars-fill.svg" alt="binoculars icon">
   <h3>Search</h3>
   <p><a href="/search?search_api_fulltext=">Find something specific</a></p>
   </div>
 
   <div class="link-block" id="block-4">
-  <img src="/modules/custom/ldbase_content/images/layout-text-window-reverse.svg">
+  <img src="/modules/custom/ldbase_content/images/layout-text-window-reverse.svg" alt="form page icon">
   <h3>Add Data</h3>
   <p><a href="/add-project">Add your project to LDbase</a></p>
   </div>
