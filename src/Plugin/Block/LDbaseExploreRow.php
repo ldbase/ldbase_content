c2c039d5a398b5b1624dd06e8b396ea0a6db1474
Brian Arsenault <barsenault@fsu.edu>
2021-05-11 16:08:33 -0400

    changes front exploration divs to links

--- src/Plugin/Block/LDbaseExploreRow.php
+++ src/Plugin/Block/LDbaseExploreRow.php
@@ -21,29 +21,34 @@ class LDbaseExploreRow extends BlockBase {
   public function build() {
     $content = <<<EOM
 <div class="browse-link-row d-flex flex-row justify-content-around flex-wrap">
-  <div class="link-block" id="block-1">
+<a class="link-div" href="/browse-data">
+ <div class="link-block" id="block-1">
   <img src="/modules/custom/ldbase_content/images/bookshelf.svg" alt="bookshelf icon">
-  <h3>Browse Data</h3>
-  <p><a class="link-div" href="/browse-data">See what's in LDbase</a></p>
+  <h3 class="link-block-header">Browse Data</h3>
+  <p>See what's in LDbase</p>
   </div>
-
+</a>
+<a class="link-div" href="/resources">
   <div class="link-block" id="block-2">
   <img src="/modules/custom/ldbase_content/images/signpost-2.svg" alt="signpost icon">
-  <h3>Resources</h3>
-  <p><a href="/resources">Learn about good data practices</a></p>
+  <h3 class="link-block-header">Resources</h3>
+  <p>Learn about good data practices</p>
   </div>
-
+</a>
+<a class="link-div" href="/search?search_api_fulltext=">
   <div class="link-block" id="block-3">
   <img src="/modules/custom/ldbase_content/images/binoculars-fill.svg" alt="binoculars icon">
-  <h3>Search</h3>
-  <p><a href="/search?search_api_fulltext=">Find something specific</a></p>
+  <h3 class="link-block-header">Search</h3>
+  <p>Find something specific</p>
   </div>
-
+</a>
+<a class="link-div" href="/add-project">
   <div class="link-block" id="block-4">
   <img src="/modules/custom/ldbase_content/images/layout-text-window-reverse.svg" alt="form page icon">
-  <h3>Add Data</h3>
-  <p><a href="/add-project">Add your project to LDbase</a></p>
+  <h3 class="link-block-header">Add Data</h3>
+  <p>Add your project to LDbase</p>
   </div>
+</a>
 </div>
 EOM;
 
