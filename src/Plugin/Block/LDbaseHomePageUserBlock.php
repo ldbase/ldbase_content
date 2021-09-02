efed199e76dafcd1a2851668cc0a3a6ad7a5af2b
Brian Arsenault <barsenault@fsu.edu>
2021-09-02 14:05:18 -0400

    adds indication that email has not been verified plus link to resend verification link

--- src/Plugin/Block/LDbaseHomePageUserBlock.php
+++ src/Plugin/Block/LDbaseHomePageUserBlock.php
@@ -6,6 +6,11 @@ use Drupal\Core\Block\BlockBase;
 use Drupal\Core\Render\Markup;
 use Drupal\Core\Link;
 use Drupal\Core\Url;
+use Drupal\Core\Entity\EntityTypeManagerInterface;
+use Drupal\Core\Plugin\ContainerFactoryPluginInterface;
+use Drupal\Core\Session\AccountInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+use Drupal\user_email_verification\UserEmailVerification;
 
 /**
  * Provides home page user block
@@ -17,24 +22,84 @@ use Drupal\Core\Url;
  * )
  */
 
- class LDbaseHomePageUserBlock extends BlockBase {
+ class LDbaseHomePageUserBlock extends BlockBase implements ContainerFactoryPluginInterface {
 
+  /**
+   * An entity type manager.
+   *
+   * @var \Drupal\Core\Entity\EntityTypeManagerInterface
+   */
+  protected $entityManager;
+
+  /**
+   * The current user.
+   *
+   * @var \Drupal\Core\Session\AccountInterface
+   */
+  protected $currentUser;
+
+  /**
+   * The User Email Verification service.
+   *
+   * @var \Drupal\user_email_verification\UserEmailVerification
+   */
+  protected $userEmailVerification;
+
+  /**
+   * Constructs a Home Page User Block
+   *
+   * @param array $configuration
+   *   Block configuration.
+   * @param string $plugin_id
+   *   The plugin ID.
+   * @param mixed $plugin_definition
+   *   The plugin definition.
+   * @param \Drupal\Core\Entity\EntityTypeManagerInterface $entity_manager
+   *   An entity type manager.
+   * @param \Drupal\Core\Session\AccountInterface $currentUser
+   *   The Current User
+   * @param \Drupal\user_email_verification\UserEmailVerification $userEmailVerification
+   *   User Email Verification Service
+   */
+  public function __construct(array $configuration, $plugin_id, $plugin_definition, EntityTypeManagerInterface $entity_manager, AccountInterface $currentUser, UserEmailVerification $userEmailVerification) {
+    parent::__construct($configuration, $plugin_id, $plugin_definition);
+    $this->entityManager = $entity_manager;
+    $this->currentUser = $currentUser;
+    $this->userEmailVerification = $userEmailVerification;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function create(ContainerInterface $container, array $configuration, $plugin_id, $plugin_definition) {
+    return new static(
+      $configuration,
+      $plugin_id,
+      $plugin_definition,
+      $container->get('entity_type.manager'),
+      $container->get('current_user'),
+      $container->get('user_email_verification.service')
+    );
+  }
+
+  /**
+   * {@inheritdoc}
+   */
   public function build() {
-    $entity_type_manager = \Drupal::entityTypeManager();
-    $current_user = \Drupal::currentUser();
     $dashboard_route = 'entity.user.canonical';
     $messages_route = 'view.my_messages.page_1';
     $contributions_route = 'entity.node.canonical';
     $profile_edit_route = 'ldbase_handlers.edit_person';
     $taxonomy_review_route = 'ldbase_handlers.review_taxonomy_terms';
-
+    $email_verification_route = 'user_email_verification.request';
+    $uid = $this->currentUser->id();
     $ldbase_person_id = \Drupal::entityQuery('node')
       ->condition('type', 'person')
-      ->condition('field_drupal_account_id', $current_user->id())
+      ->condition('field_drupal_account_id', $uid)
       ->execute();
 
     if (!empty($ldbase_person_id)) {
-      $ldbase_person = $entity_type_manager->getStorage('node')->load(array_values($ldbase_person_id)[0]);
+      $ldbase_person = $this->entityManager->getStorage('node')->load(array_values($ldbase_person_id)[0]);
 
       $person_first_name = $ldbase_person->field_first_name->value;
       $person_middle_name = $ldbase_person->field_middle_name->value;
@@ -48,25 +113,28 @@ use Drupal\Core\Url;
       $institutions = $ldbase_person->field_related_organizations->getValue();
       $person_related_organizations = [];
       foreach ($institutions as $value) {
-        $name = $entity_type_manager->getStorage('node')->load($value['target_id'])->getTitle();
+        $name = $this->entityManager->getStorage('node')->load($value['target_id'])->getTitle();
         array_push($person_related_organizations, $name);
       }
 
-      $dashboard_url = Url::fromRoute($dashboard_route, ['user' => $current_user->id()]);
+      $dashboard_url = Url::fromRoute($dashboard_route, ['user' => $uid]);
       $dashboard_link = Link::fromTextAndUrl(t('Go to Dashboard'), $dashboard_url)->toRenderable();
-      $messages_url = Url::fromRoute($messages_route, ['user' => $current_user->id()]);
+      $messages_url = Url::fromRoute($messages_route, ['user' => $uid]);
       $messages_link = Link::fromTextAndUrl(t('View Messages'), $messages_url)->toRenderable();
       $contributions_url = Url::fromRoute($contributions_route, ['node' => $ldbase_person->id()]);
       $contributions_link = Link::fromTextAndUrl(t('View Contributions'), $contributions_url)->toRenderable();
       $profile_edit_url = Url::fromRoute($profile_edit_route, ['node' => $ldbase_person->uuid()]);
       $profile_edit_link = Link::fromTextAndUrl(t('Edit your Profle'), $profile_edit_url)->toRenderable();
       $taxonomy_review_url = Url::fromRoute($taxonomy_review_route);
-      if ($taxonomy_review_url->access($current_user)) {
+      if ($taxonomy_review_url->access($this->currentUser)) {
         $taxonomy_review_link = Link::fromTextAndUrl(t('Review Taxonomy Terms'), $taxonomy_review_url)->toRenderable();
       }
       else {
         $taxonomy_review_link = NULL;
       }
+      $email_needs_verification = $this->userEmailVerification->isVerificationNeeded($uid);
+      $verification_url = Url::fromRoute($email_verification_route);
+      $verification_link = Link::fromTextAndUrl(t('Resend Verification Email'), $verification_url)->toRenderable();
 
 
       $cache_tags = [
@@ -85,9 +153,11 @@ use Drupal\Core\Url;
         '#contributions_link' => $contributions_link,
         '#profile_edit_link' => $profile_edit_link,
         '#taxonomy_review_link' => $taxonomy_review_link,
+        '#email_needs_verification' => $email_needs_verification,
+        '#verification_link' => $verification_link,
         '#cache' => [
           'tags' => $cache_tags,
-          'contexts' => ['user'],
+          'contexts' => ['user','user_email_verification_needed'],
         ],
       ];
 
