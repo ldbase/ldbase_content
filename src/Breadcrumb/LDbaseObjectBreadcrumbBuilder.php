a9d87bcc60091b3daf0785b8002372be4b334c7e
Brian Arsenault <barsenault@fsu.edu>
2021-09-30 11:57:23 -0400

    replaces call to deprecated function entity_load()

--- src/Breadcrumb/LDbaseObjectBreadcrumbBuilder.php
+++ src/Breadcrumb/LDbaseObjectBreadcrumbBuilder.php
@@ -4,15 +4,44 @@ namespace Drupal\ldbase_content\Breadcrumb;
 
 use Drupal\Core\Breadcrumb\Breadcrumb;
 use Drupal\Core\Breadcrumb\BreadcrumbBuilderInterface;
+use Drupal\Core\Entity\EntityTypeManagerInterface;
 use Drupal\Core\Routing\RouteMatchInterface;
 use Drupal\Core\Link;
-
+use Drupal\ldbase_content\LDbaseObjectService;
 
 class LDbaseObjectBreadcrumbBuilder implements BreadcrumbBuilderInterface {
 
+  /**
+   * The entity type manager.
+   *
+   * @var \Drupal\Core\Entity\EntityTypeManagerInterface
+   */
+  protected $entityTypeManager;
+
+  /**
+   * The LDbase Object service.
+   *
+   * @var \Drupal\ldbase_content\LDbaseObjectService
+   */
+  protected $ldbaseObjectService;
+
+  /**
+   * Constructs an LDbase Object Breadcrumb builder
+   *
+   * @param \Drupal\Core\Entity\EntityTypeManagerInterface $entity_type_manager
+   *  The entity type manager
+   * @param \Drupal\ldbase_content\LDbaseObjectService $ldbase_object_service
+   *  The LDBase Object Service
+   *
+   */
+  public function __construct(EntityTypeManagerInterface $entity_type_manager, LDbaseObjectService $ldbase_object_service) {
+    $this->entityTypeManager = $entity_type_manager;
+    $this->ldbaseObjectService = $ldbase_object_service;
+  }
+
   public function applies(RouteMatchInterface $route_match) {
     $url = \Drupal::request()->getRequestUri();
-    $ldbase_object_uuid = \Drupal::service('ldbase.object_service')->isUrlAnLdbaseObjectUrl($url);
+    $ldbase_object_uuid = $this->ldbaseObjectService->isUrlAnLdbaseObjectUrl($url);
     if ($ldbase_object_uuid) {
       return TRUE;
     }
@@ -26,9 +55,9 @@ class LDbaseObjectBreadcrumbBuilder implements BreadcrumbBuilderInterface {
     $breadcrumb->addCacheContexts(['url.path', 'route']);
     $breadcrumb->addLink(Link::createFromRoute('Home', '<front>'));
     $url = \Drupal::request()->getRequestUri();
-    $ldbase_object_uuid = \Drupal::service('ldbase.object_service')->isUrlAnLdbaseObjectUrl($url);
-    $ldbase_object = \Drupal::service('ldbase.object_service')->getLdbaseObjectFromUuid($ldbase_object_uuid);
-    $breadcrumb_trail = \Drupal::service('ldbase.object_service')->getBreadcrumbTrailToLdbaseObject(array(array('title' => $ldbase_object->getTitle(), 'nid' => $ldbase_object->id())));
+    $ldbase_object_uuid = $this->ldbaseObjectService->isUrlAnLdbaseObjectUrl($url);
+    $ldbase_object = $this->ldbaseObjectService->getLdbaseObjectFromUuid($ldbase_object_uuid);
+    $breadcrumb_trail = $this->ldbaseObjectService->getBreadcrumbTrailToLdbaseObject(array(array('title' => $ldbase_object->getTitle(), 'nid' => $ldbase_object->id())));
 
     $url_bits_count = count(explode('/', $url));
     if ($url_bits_count <= 3) {
@@ -36,8 +65,8 @@ class LDbaseObjectBreadcrumbBuilder implements BreadcrumbBuilderInterface {
     }
 
     foreach ($breadcrumb_trail as $breadcrumb_link) {
-      $entity = entity_load('node', $breadcrumb_link['nid']);
-      $bundle = \Drupal::service('ldbase.object_service')->isLdbaseCodebook($entity->uuid()) ? 'codebook' : $entity->bundle();
+      $entity = $this->entityTypeManager->getStorage('node')->load($breadcrumb_link['nid']);
+      $bundle = $this->ldbaseObjectService->isLdbaseCodebook($entity->uuid()) ? 'codebook' : $entity->bundle();
       $formatted_bundle = ucfirst($bundle);
       $formatted_title = "{$formatted_bundle}: {$breadcrumb_link['title']}";
       $breadcrumb->addLink(Link::createFromRoute($formatted_title, 'entity.node.canonical', ['node' => $breadcrumb_link['nid']], ['absolute' => TRUE]));
